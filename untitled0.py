99e0b282c696b0bea5e9063991074b7155416937
mqzyiv <159936345+mqzyiv@users.noreply.github.com>
2025-04-12 16:30:52 -0400

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -7,36 +7,27 @@ Original file is located at
     https://colab.research.google.com/drive/1lXUbszNrTmVjkZ21f-sS_AcFJ5A_y9n4
 """
 
-from google.colab import userdata
 from google import genai
-client = genai.Client(api_key =userdata.get('GOOGLE_API_KEY') )
-
-sample_pdf = client.files.upload(file="/content/gemini.pdf")
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents=["generate three quiz questions based on this pdf and return questions and multiple choice as a json file with questions named question and options named options and the answer named answer", sample_pdf],
-)
-print(response.text)
-
-text = response.text
-text = text[len("```json"):]
-text = text[:text.index("```")]
+client = genai.Client(api_key ='[UR API KEY]')
 
 import json
-
-data =json.loads(text)
-for i in data:
-
+def fun(txt):
   response = client.models.generate_content(
       model="gemini-2.0-flash",
-      contents=["can you return a quote directly from this pdf that contains the answer to" + i['question'], sample_pdf],
+      contents=["generate three quiz questions based on this text and return questions and multiple choice as a json file with questions named question and options named options and the answer named answer", txt],
   )
-  print(response.text)
+  text = response.text
+  text = text[len("```json"):]
+  text = text[:text.index("```")]
+  data= json.loads(text)
+  for i in data:
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=["can you return a sentence directly from this text that contains the answer to" + i['question'],txt]
+        )
+    i["highlight"] = response.text
+  return data
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents=["can you return a quote directly from this pdf that contains the answer to <In what year did scientists discover a 6,000-year-old winery in an Armenian cave?>", sample_pdf],
-)
-print(response.text)
+text = "One of the first organisms that humans domesticated was yeast. In 2011, while excavating an old graveyard in an Armenian cave, scientists discovered a 6,000 year-old winery, complete with a wine press, fermentation vessels, and even drinking cups. This winery was a major technological innovation that required understanding how to control Sacharomyces, the genus of yeast used in alcohol and bread production."
+print(fun(text))
 
